63f6892b7b196d1ec8903e45c19921bf26381f40
as9322 <as9322@ic.ac.uk>
2025-02-24 13:51:35 +0000

    Lab 1 Complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <bitset>
+#include <STM32FreeRTOS.h>
 
 //Constants
   const uint32_t interval = 100; //Display update interval
+  const uint32_t stepSizes [] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
+  const uint32_t StepSizeFactor = 195226;
+  const char* noteNames[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+
+//Global Variables
+volatile uint32_t currentStepSize = 0;
+const char* currentNote;
+struct {
+  std::bitset<32> inputs;  
+  } sysState;
+
+//Global Timer
+HardwareTimer sampleTimer(TIM1);
+
 
 //Pin definitions
   //Row select and enable
@@ -35,6 +51,36 @@
 //Display driver object
 U8G2_SSD1305_128X32_ADAFRUIT_F_HW_I2C u8g2(U8G2_R0);
 
+void setRow(uint8_t rowIdx){
+      digitalWrite(REN_PIN, LOW);
+    
+      digitalWrite(RA0_PIN, rowIdx & 0x01);
+      digitalWrite(RA1_PIN, rowIdx & 0x02);
+      digitalWrite(RA2_PIN, rowIdx & 0x04);
+      
+      digitalWrite(REN_PIN, HIGH);
+}
+
+//Function to read inputs from four columns of the switch matrix
+std::bitset<4> readCols(){
+  std::bitset<4> result;
+  
+  result[0] = digitalRead(C0_PIN);
+  result[1] = digitalRead(C1_PIN);
+  result[2] = digitalRead(C2_PIN);
+  result[3] = digitalRead(C3_PIN);
+
+  return result;
+  }
+
+//ISR that generates output waveform
+void sampleISR() {
+  static uint32_t phaseAcc = 0;
+  phaseAcc += currentStepSize;
+  int32_t Vout = (phaseAcc >> 24) - 128;
+  analogWrite(OUTL_PIN, Vout+128);
+}
+
 //Function to set outputs using key matrix
 void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,LOW);
@@ -47,8 +93,64 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,LOW);
 }
 
+void scanKeysTask(void * pvParameters) {
+  const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  while(1){
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+    uint32_t localCurrentStepSize = 0;
+    //__atomic_load_n(&currentStepSize, localCurrentStepSize);
+    for(int row=0; row<3; row++){
+      setRow(row);
+      delayMicroseconds(3);
+      std::bitset<4> cols = readCols();
+      sysState.inputs[row * 4 + 0] = cols[0];
+      sysState.inputs[row * 4 + 1] = cols[1];
+      sysState.inputs[row * 4 + 2] = cols[2];
+      sysState.inputs[row * 4 + 3] = cols[3];
+
+      for (int col = 0; col < 4; col++) {
+        if (!cols[col]) {
+          localCurrentStepSize = stepSizes[row * 4 + col]*StepSizeFactor;
+          currentNote = noteNames[row * 4 + col];
+        }
+      }
+    }
+    __atomic_store_n(&currentStepSize, localCurrentStepSize, __ATOMIC_RELAXED);
+  }
+}
+
+//Update display
+void displayUpdateTask(void * pvParameters) {
+  const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  while(1){
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+    u8g2.clearBuffer();         // clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+    u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
+    u8g2.setCursor(2,20);
+    u8g2.print(sysState.inputs.to_ulong(),HEX); 
+    u8g2.setCursor(2,30);
+    if (sysState.inputs == 0xFFF) {
+      u8g2.print("No Note");
+    } else {
+      u8g2.print("Note: ");
+      u8g2.print(currentNote);
+    }
+    u8g2.sendBuffer();          // transfer internal memory to the display
+    //Toggle LED
+    digitalToggle(LED_BUILTIN);
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
+
+  //Init Timer and attach ISR
+  sampleTimer.setOverflow(22000, HERTZ_FORMAT);
+  sampleTimer.attachInterrupt(sampleISR);
+  sampleTimer.resume();
 
   //Set pin directions
   pinMode(RA0_PIN, OUTPUT);
@@ -77,26 +179,28 @@ void setup() {
   //Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
+
+  //Initialise RTOS Scheduler
+  TaskHandle_t scanKeysHandle = NULL;
+  xTaskCreate(
+    scanKeysTask,		/* Function that implements the task */
+    "scanKeys",		/* Text name for the task */
+    64,      		/* Stack size in words, not bytes */
+    NULL,			/* Parameter passed into the task */
+    2,			/* Task priority */
+    &scanKeysHandle );	/* Pointer to store the task handle */
+  TaskHandle_t displayUpdateHandle = NULL;
+  xTaskCreate(
+    displayUpdateTask,		/* Function that implements the task */
+    "displayUpdate",		/* Text name for the task */
+    256,      		/* Stack size in words, not bytes */
+    NULL,			/* Parameter passed into the task */
+    1,			/* Task priority */
+    &displayUpdateHandle );	/* Pointer to store the task handle */
+  vTaskStartScheduler();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  static uint32_t count = 0;
-
-  while (millis() < next);  //Wait for next interval
-
-  next += interval;
-
-  //Update display
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(count++);
-  u8g2.sendBuffer();          // transfer internal memory to the display
-
-  //Toggle LED
-  digitalToggle(LED_BUILTIN);
   
 }
